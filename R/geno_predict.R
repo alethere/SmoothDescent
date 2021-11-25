43c9f42fcdb280ee43339223d9d298733b23d7ab
Therese Navarro, Alejandro <alejandro.theresenavarro@wur.nl>
2021-11-25 11:35:24 +0100

    Changed threshold behaviour to reflect probability more accurately.

--- R/geno_predict.R
+++ R/geno_predict.R
@@ -2,6 +2,7 @@
 
 #Function to calculate genotypes from a single individual
 #ibd matrix and a homologue matrix
+
 #' Genotype calculator
 #'
 #' Given a set of IBD probabilities, genotypes are calculated. Can take as input a single
@@ -20,7 +21,7 @@
 #' @details Genotypes can be though of as a multiplication between a set of homologue probabilites
 #' and homologue genotypes. For example, in a diploid we have the following IBD probabilities:
 #' p(h1) = 0.9, p(h2) = 0.1, p(h3) = 1, p(h4) = 0; and genotypes g(h1) = 1, g(h2) = 0, g(h3) = 0, g(h4) = 1.
-#' The resulting genotype using a simple multiplication would be \sum{p(hi)*g(hi)}, 0.9 in this case.\break
+#' The resulting genotype using a simple multiplication would be \eqn{\sum{p(hi)*g(hi)}}, 0.9 in this case.\cr
 #' We are more interested in imputing integer genotypes, so probabilities must be turned into
 #' integers. For that reason we use a threshold, default 0.8. All probabilities above 0.8 are turned to 1,
 #' and all probabilities below 0.8 are turned to 0. If not enough homologues are assigned this way,
@@ -32,6 +33,16 @@
 #' @export
 #'
 #' @examples
+#' data("smooth_descent")
+#' data("homologue")
+#' IBD <- sdescent$predIBD
+#'
+#' #For all individuals at once
+#' gen <- genotype(IBD, hom)
+#' #Or one single individual
+#' #Note this is a matrix where each column is one homologue
+#' ind <- sapply(IBD,'[',,1)
+#' gen <- genotype(ind, hom)
 genotype <- function(IBD,homologue,threshold = 0.8, ploidy = 2){
   UseMethod("genotype",IBD)
 }
@@ -40,29 +51,27 @@ genotype <- function(IBD,homologue,threshold = 0.8, ploidy = 2){
 #'
 #' @describeIn genotype Method for a single individual
 genotype.matrix <- function(IBD,homologue,threshold = 0.8,ploidy = 2){
-  # if(is.matrix(IBD)){
-  #   assertthat::assert_that(all(colnames(homologue) %in% colnames(IBD)))
-  # }else{
-  #   assertthat::assert_that(all(colnames(homologue) %in% names(IBD)))
-  # }
-
-  #Above threshold are turned into 1
-  IBD[IBD >= threshold] <- 1
-  IBD[IBD <= 1-threshold] <- 0
+  # #Above threshold are turned into 1
+  # IBD[IBD >= threshold] <- 1
+  # IBD[IBD <= 1-threshold] <- 0
 
   marks <- intersect(rownames(homologue),rownames(IBD))
   IBD <- IBD[marks,]
   homologue <- homologue[marks,]
   #Genotypes are calculated
   perhom_geno <- IBD*homologue
-  not_certain <- (perhom_geno != 0 & perhom_geno != 1)
-  not_certain <- apply(not_certain, 1, any) | rowSums(IBD) != ploidy
   geno <- rowSums(perhom_geno)
+  not_certain <- rowSums(IBD)/ploidy < threshold
 
   #There must be two columns per individual over the threshold
   #This is somehow problematic though, what if two columns of p1 instead of 1 in p1 and one in p2?
   # not_certain <- rowSums(IBD > threshold) < ploidy
+  # not_certain <- (perhom_geno != 0 & perhom_geno != 1)
+  # not_certain <- apply(not_certain, 1, any) | rowSums(IBD) != ploidy
+  # decimals <- geno %% 1
+  # not_certain <- decimals > threshold | decimals < threshold
   geno[not_certain] <- NA
+  geno <- round(geno)
 
   return(geno)
 }
@@ -72,9 +81,6 @@ genotype.matrix <- function(IBD,homologue,threshold = 0.8,ploidy = 2){
 #' @describeIn genotype Method for a list of IBD matrices
 genotype.list <- function(IBD,homologue,threshold = 0.8, ploidy = 2){
   if(!all(colnames(homologue) %in% names(IBD))) stop("IBD list should contain one element per column in homologue matrix")
-
-  parentcols <- list(p1 = 1:ploidy,
-                     p2 = (ploidy + 1):(ploidy*2))
 
   ind_ibd <- lapply(1:ncol(IBD[[1]]),function(i){
     ib <- sapply(IBD,'[',,i)
